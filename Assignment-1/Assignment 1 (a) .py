b6ebf56799987df9c0289e10f7f66c0528a1af6b
Yash Dhawan <dhawanyash01@gmail.com>
2025-08-26 02:03:57 +0530

    Add files via upload

---
+++ Assignment-1/Assignment 1 (a) .py
@@ -0,0 +1,7 @@
+a= int(input("Enter the first number"))
+b=int(input("enter the second number"))
+add=a+b
+difference=a-b
+product=a*b
+division=a/b
+print("Addition:", add ,"\nSubtraction:", difference ,"\nMultiplication:", product ,"\nDivision:", division)
